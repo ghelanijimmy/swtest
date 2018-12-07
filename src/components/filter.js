5968a2c7eabdf817790a3e548206d8adbc421ee1
Jimmy Ghelani <ghelanijimmy@jgdesigns.ca>
2018-12-07 16:31:06 -0500

    firebase upload and styling for filter box

--- src/components/filter.js
+++ src/components/filter.js
@@ -3,12 +3,21 @@ import React, {Component} from 'react';
 class Filter extends Component {
 
     render(){
-        return(
-            <div>
-                <input id={'filterInput'} placeholder={'Filter'} onChange={this.props.onFilter} value={this.props.filter}/>
-                <div id={'filterOutput'}>{this.props.filter}</div>
-            </div>
-        );
+        if(this.props.filter == ""){
+            return(
+                <div className={'filterBox'}>
+                    <input id={'filterInput'} placeholder={'Filter'} onChange={this.props.onFilter} value={this.props.filter}/>
+                    <div id={'filterOutput'}>{this.props.filter}</div>
+                </div>
+            );
+        }else {
+            return (
+                <div className={'filterBox'}>
+                    <input id={'filterInput'} placeholder={'Filter'} onChange={this.props.onFilter} value={this.props.filter}/>
+                    <div id={'filterOutput'}><span id={"filterLabel"}>Filtering for: </span>{this.props.filter}</div>
+                </div>
+            )
+        }
     }
 }
 
